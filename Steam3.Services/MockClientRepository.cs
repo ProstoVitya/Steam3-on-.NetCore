fdd526eda4c3a30da20e2533aeaab5e11b4137d0
ProstoVitya <79058360+ProstoVitya@users.noreply.github.com>
2022-05-14 20:10:53 +0400

    added notes

--- Steam3.Services/MockClientRepository.cs
+++ Steam3.Services/MockClientRepository.cs
@@ -10,14 +10,7 @@ namespace Steam3.Services
         {
             _clientList = new List<Client>()
             {
-                new Client{ Login = "Login1", Password = "Password1", Name = "Name1", CreditCard = 112,
-                    AvalibleGames =
-                    {
-                        new AvalibleGame { UserLogin = "Login1", GameName = "TestGame" },
-                        new AvalibleGame { UserLogin = "Login1", GameName = "Grido Islando" },
-                        new AvalibleGame { UserLogin = "Login1", GameName = "Elden Ring" }
-                    }
-                },
+                new Client{ Login = "Login1", Password = "Password1", Name = "Name1", CreditCard = 112 },
                 new Client{ Login = "Login2", Password = "Password2", Name = "Name2", CreditCard = 212 },
                 new Client{ Login = "Login3", Password = "Password3", Name = "Name3", CreditCard = 312 },
                 new Client{ Login = "Login4", Password = "Password4", Name = "Name4", CreditCard = 412 },
@@ -35,11 +28,18 @@ namespace Steam3.Services
             return newClient;
         }
 
+        //Add cascade deleting
         public Client Delete(string login)
         {
             var clientToDelete = _clientList.Find(c => c.Login.Equals(login, StringComparison.OrdinalIgnoreCase));
             if (clientToDelete != null)
+            {
+                foreach (var avalibleGame in clientToDelete.AvalibleGames)
+                {
+                    
+                }
                 _clientList.Remove(clientToDelete);
+            }
             return clientToDelete;
         }
 
@@ -61,6 +61,7 @@ namespace Steam3.Services
                                  || c.Login.Contains(searchTerm, StringComparison.OrdinalIgnoreCase));
         }
 
+        //add cascade update
         public Client Update(string login, Client updatedClient)
         {
             var clientWithTheSameLogin = _clientList.Find(c => c.Login.Equals(updatedClient.Login));
@@ -70,6 +71,11 @@ namespace Steam3.Services
             var clientToUpdate = _clientList.Find(c => c.Login.Equals(login));
             if (clientToUpdate != null)
             {
+               /* IAvalibleGameRepository avalible = new MockAvalibleGameRepository();
+                foreach (var avalibleGame in avalible.GetGames(clientToUpdate.Login))
+                {
+                    avalibleGame.UserLogin = updatedClient.Login;
+                }*/
                 clientToUpdate.Login = updatedClient.Login;
                 clientToUpdate.Password = updatedClient.Password;
                 clientToUpdate.CreditCard=updatedClient.CreditCard;
