e1b1c4e21ec131500e13562b477b0dff97bd577d
ProstoVitya <79058360+ProstoVitya@users.noreply.github.com>
2022-05-18 23:25:13 +0400

    purchase functionality
    Added actions for the Buy buttons

--- Steam3.RazorPages/Pages/Index.cshtml.cs
+++ Steam3.RazorPages/Pages/Index.cshtml.cs
@@ -8,14 +8,23 @@ namespace Steam3.RazorPages.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly IGameRepository _db;
+        private readonly IGameRepository _gameRepository;
+        private readonly IAvalibleGameRepository _avalibleGameRepository;
+        private readonly ICreditCardRepository _creditCardRepository;
+        private readonly IClientRepository _clientRepository;
 
-        public IndexModel(ILogger<IndexModel> logger, IGameRepository db)
+        public IndexModel(ILogger<IndexModel> logger, IGameRepository gameRepository,
+            IAvalibleGameRepository avalibleGameRepository, ICreditCardRepository creditCardRepository,
+            IClientRepository clientRepository)
         {
             _logger = logger;
-            _db = db;
+            _gameRepository = gameRepository;
+            _avalibleGameRepository = avalibleGameRepository;
+            _creditCardRepository = creditCardRepository;
+            _clientRepository = clientRepository;
         }
 
+        [BindProperty]
         public IEnumerable<Game> Games { get; set; }
 
         [BindProperty(SupportsGet = true)]
@@ -23,12 +32,54 @@ namespace Steam3.RazorPages.Pages
 
         public void OnGet()
         {
-            Games = _db.Search(SearchTerm);
+            Games = _gameRepository.Search(SearchTerm);
         }
 
-        public IActionResult OnPostTest(string gameName)
+        public IActionResult OnPostBuy(string gameName, int cost)
         {
-            return RedirectToPage("NotFound");
+            if (StaticVariables.IsAdmin)
+            {
+                TempData["SuccessMessage"] = "Зайдите за обычного пользователя";
+                return Page();
+            }
+            if (string.IsNullOrWhiteSpace(StaticVariables.Login))
+                return RedirectToPage("/ClientPages/Login");
+           if(!HasGame(gameName))
+                return Buy(gameName, cost);
+            TempData["SuccessMessage"] = $"У вас уже есть {gameName}";
+            return RedirectToPage("Index");
+        }
+
+        private bool HasGame(string gameName)
+        {
+            var clientAvalibleGames = _avalibleGameRepository.GetGamesByUser(StaticVariables.Login);
+            foreach (var game in clientAvalibleGames)
+                if (game.GameName == gameName)
+                    return true;
+
+            return false;
+        }
+
+        private IActionResult Buy(string gameName, int cost)
+        {
+            var client = _clientRepository.GetClient(StaticVariables.Login);
+            var credtCard = _creditCardRepository.GetCard(client.CreditCard);
+            if (credtCard.Money < cost)
+            {
+                TempData["SuccessMessage"] = "У вас недостаточно денег";
+                return RedirectToPage("Index");
+
+            }
+            credtCard.Money -= cost;
+            _avalibleGameRepository.Add(
+                new AvalibleGame
+                {
+                    GameName = gameName,
+                    UserLogin = StaticVariables.Login
+                });
+            TempData["SuccessMessage"] = $"{gameName} была успешно куплена и добавлена в библиотеку";
+            return RedirectToPage("Index");
+
         }
     }
 }
